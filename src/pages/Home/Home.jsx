c706d15be7f97ba87fbd6a8cf56dfde9d1037a29
Steven <scarvajalusb@gmail.com>
2020-07-13 09:01:08 -0500

    Responsive img in home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import logo from "../../utils/img/pngwave.png"
 
 const Home = () => {
     return ( 
         <div className="Component-home">
+            <div>
             <h1 className="Steven">Hola ! </h1>
             <h1 className="Steven">Mi nombre es Steven</h1>
             <h2 className="profesion">Ingeniero de software</h2>
@@ -11,7 +13,8 @@ const Home = () => {
             <p>Pagina creada con:</p>
             <br/>
             <p>React / Html / Css / JavaScript</p>
-            <img src="src\utils\img\pngwave.png" alt=""/>
+            </div>
+            <img src={logo} alt=""/>
         </div>
      );
 }
